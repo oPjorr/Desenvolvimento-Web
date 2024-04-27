81edd143f6bc2f80bd245852ec47c75c73598db0
Paulo Rolinski <paulorolinski.ifc@gmail.com>
2024-04-26 23:35:12 -0300

    Utilizando v-bind

---
+++ Vue/intro_vue_3/t3-v-bind/main.js
@@ -0,0 +1,9 @@
+const app = Vue.createApp({
+    data() {
+        return {
+            product: 'Socks',
+            image: './assets/images/socks_green.jpg',
+            url: 'https://github.com/csp1po/intro_vue_3/tree/t3-start'
+        }
+    }
+})
