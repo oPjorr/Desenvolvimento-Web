9e4f42eaac3c33c8919d6fcd99189f108cbef6bc
Paulo Rolinski <paulorolinski.ifc@gmail.com>
2024-07-05 16:08:00 -0300

    t3-start

--- Vue/intro_vue_3-v2.0/main.js
+++ Vue/intro_vue_3-v2.0/main.js
@@ -2,7 +2,8 @@ const app = Vue.createApp({
     data() {
         return {
             product: 'Socks',
-            description: 'Meias do melhor estilo! Estilo Framework!'
+            image: "./assets/images/socks_green.jpg",
+            url: "https://pt.aliexpress.com/item/1005005041940957.html"
         }
     }
 })
