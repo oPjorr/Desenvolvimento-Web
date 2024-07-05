9f2393075fc4abcb4f7ebe68b2e1db1e768c3eb4
Paulo Rolinski <paulorolinski.ifc@gmail.com>
2024-07-05 16:20:34 -0300

    t4-start

--- Vue/intro_vue_3-v2.0/main.js
+++ Vue/intro_vue_3-v2.0/main.js
@@ -2,8 +2,9 @@ const app = Vue.createApp({
     data() {
         return {
             product: 'Socks',
-            image: "./assets/images/socks_green.jpg",
-            url: "https://pt.aliexpress.com/item/1005005041940957.html"
+            image: './assets/images/socks_blue.jpg',
+            inventory: 8,
+            onSale: true
         }
     }
 })
