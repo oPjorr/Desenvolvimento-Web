aa372ad8685b0dd03c8414d013a9f2d48e5703e4
Paulo Rolinski <paulorolinski.ifc@gmail.com>
2024-08-09 18:25:21 -0300

    feat(real-world-vue): tutorial 6

--- Vue/vue3_mundo_real/real-world-vue/src/router/index.js
+++ Vue/vue3_mundo_real/real-world-vue/src/router/index.js
@@ -1,13 +1,14 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import EventListView from '../views/EventListView.vue'
 import AboutView from '../views/AboutView.vue'
+import EventDetailsView from '@/views/EventDetailsView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
-      name: 'event-list',
+      name: 'home',
       component: EventListView,
     },
     {
@@ -15,6 +16,12 @@ const router = createRouter({
       name: 'about',
       component: AboutView,
     },
+    {
+      path: '/event/:id',
+      name: 'event-details',
+      props: true,
+      component: EventDetailsView
+    }
   ],
 })
 
